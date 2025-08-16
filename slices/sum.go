46c7f3a130819beb19a09a9533809d3e07dbe3a1
Borriguel <rodolpho-henrique@hotmail.com.br>
2025-08-16 17:14:42 -0300

    adiciona funções SumAll e SumLast com testes correspondentes

--- slices/sum.go
+++ slices/sum.go
@@ -6,3 +6,22 @@ func Sum(nums []int) (sum int) {
 	}
 	return
 }
+
+func SumAll(nums ...[]int) (sum []int) {
+	for _, num := range nums {
+		sum = append(sum, Sum(num))
+	}
+	return
+}
+
+func SumLast(nums ...[]int) (sum []int) {
+	for _, num := range nums {
+		if len(num) == 0 {
+			sum = append(sum, 0)
+		} else {
+			final := num[1:]
+			sum = append(sum, Sum(final))
+		}
+	}
+	return
+}
--- slices/sum_test.go
+++ slices/sum_test.go
@@ -1,6 +1,9 @@
 package slices
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("colection of 5 nums", func(t *testing.T) {
@@ -18,6 +21,30 @@ func TestSum(t *testing.T) {
 		expected := 55
 		if result != expected {
 			t.Errorf("result '%d', expected '%d', given '%v'", result, expected, nums)
+		}
+	})
+
+	t.Run("sum all collections", func(t *testing.T) {
+		result := SumAll([]int{3, 9}, []int{16, 1, 4})
+		expected := []int{12, 21}
+		if !reflect.DeepEqual(result, expected) {
+			t.Errorf("result '%d', expected '%d'", result, expected)
+		}
+	})
+
+	t.Run("sum last num of collection", func(t *testing.T) {
+		result := SumLast([]int{0, 5}, []int{12, 0, -3})
+		expected := []int{5, -3}
+		if !reflect.DeepEqual(result, expected) {
+			t.Errorf("result '%d', expected '%d'", result, expected)
+		}
+	})
+
+	t.Run("sum empty slices", func(t *testing.T) {
+		result := SumLast([]int{}, []int{0, -8})
+		expected := []int{0, -8}
+		if !reflect.DeepEqual(result, expected) {
+			t.Errorf("result '%d', expected '%d'", result, expected)
 		}
 	})
 }
