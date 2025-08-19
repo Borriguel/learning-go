eedaebfd7aac85a5ef93e23a22a481127fb50f16
Borriguel <rodolpho-henrique@hotmail.com.br>
2025-08-19 19:23:56 -0300

    adiciona tratamento de erro ao método Search do Dictionary e implementa testes correspondentes

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,7 +1,15 @@
 package maps
 
+import "errors"
+
 type Dictionary map[string]string
 
-func (d Dictionary) Search(word string) string {
-	return d[word]
+var ErrNotFind = errors.New("could not find the word you are looking for")
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, exists := d[word]
+	if !exists {
+		return "", ErrNotFind
+	}
+	return definition, nil
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -4,14 +4,27 @@ import "testing"
 
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
-	result := dictionary.Search("test")
-	expected := "this is just a test"
-	compareStrings(t, result, expected)
+	t.Run("known word", func(t *testing.T) {
+		result, _ := dictionary.Search("test")
+		expected := "this is just a test"
+		compareStrings(t, result, expected)
+	})
+	t.Run("unknown word", func(t *testing.T) {
+		_, result := dictionary.Search("unknown")
+		compareError(t, result, ErrNotFind)
+	})
 }
 
 func compareStrings(t *testing.T, result, expected string) {
 	t.Helper()
 	if result != expected {
 		t.Errorf("result '%s', expected '%s', given '%s'", result, expected, "test")
+	}
+}
+
+func compareError(t *testing.T, result, expected error) {
+	t.Helper()
+	if result != expected {
+		t.Errorf("result '%s', expected '%s'", result, expected)
 	}
 }
