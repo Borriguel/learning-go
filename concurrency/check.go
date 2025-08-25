968a67217dcaeb1c8e9b1b2cc64b708543f01e3e
Borriguel <rodolpho-henrique@hotmail.com.br>
2025-08-25 20:52:48 -0300

    refatora a função WebsiteCheck para utilizar goroutines e canais, melhorando a concorrência

--- concurrency/check.go
+++ concurrency/check.go
@@ -1,11 +1,22 @@
 package concurrency
 
 type WebsiteChecker func(url string) bool
+type result struct {
+	string
+	bool
+}
 
-func WebsiteCheck(wc WebsiteChecker, url []string) map[string]bool {
+func WebsiteCheck(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
-	for _, url := range url {
-		results[url] = wc(url)
+	channelResult := make(chan result)
+	for _, url := range urls {
+		go func(u string) {
+			channelResult <- result{u, wc(u)}
+		}(url)
+	}
+	for i := 0; i < len(urls); i++ {
+		result := <-channelResult
+		results[result.string] = result.bool
 	}
 	return results
 }
