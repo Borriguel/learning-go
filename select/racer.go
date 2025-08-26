4463616d3ff033706b34a55ec64fe3d57982b25b
Borriguel <rodolpho-henrique@hotmail.com.br>
2025-08-26 20:28:15 -0300

    adiciona suporte à configuração de timeout na função Racer e refatora testes para cobrir novos cenários

--- select/racer.go
+++ select/racer.go
@@ -1,15 +1,25 @@
 package _select
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 )
 
-func Racer(x, y string) (winner string) {
+var limit = 10 * time.Second
+
+func Racer(x, y string) (winner string, err error) {
+	return Configurable(x, y, limit)
+}
+
+func Configurable(x, y string, limit time.Duration) (winner string, err error) {
 	select {
 	case <-ping(x):
-		return x
+		return x, nil
 	case <-ping(y):
-		return y
+		return y, nil
+	case <-time.After(limit):
+		return "", fmt.Errorf("timeout exceeded for %q and %q", x, y)
 	}
 }
 
--- select/racer_test.go
+++ select/racer_test.go
@@ -8,17 +8,30 @@ import (
 )
 
 func TestRacer(t *testing.T) {
-	slowServer := createServerWithDelay(20)
-	fastServer := createServerWithDelay(0)
-	defer slowServer.Close()
-	defer fastServer.Close()
-	slowUrl := slowServer.URL
-	fastUrl := fastServer.URL
-	want := fastUrl
-	got := Racer(slowUrl, fastUrl)
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+	t.Run("compares the speed of servers, returning the address of the fastest", func(t *testing.T) {
+		slowServer := createServerWithDelay(20)
+		fastServer := createServerWithDelay(0)
+		defer slowServer.Close()
+		defer fastServer.Close()
+		slowUrl := slowServer.URL
+		fastUrl := fastServer.URL
+		want := fastUrl
+		got, err := Racer(slowUrl, fastUrl)
+		if err != nil {
+			t.Fatalf("didn't expect an error, but got one %q", err)
+		}
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+	t.Run("returns an error if the server does not respond within 10s", func(t *testing.T) {
+		server := createServerWithDelay(25 * time.Millisecond)
+		defer server.Close()
+		_, err := Configurable(server.URL, server.URL, 5*time.Millisecond)
+		if err == nil {
+			t.Errorf("expected an error, but I didn't get one")
+		}
+	})
 }
 
 func createServerWithDelay(delay time.Duration) *httptest.Server {
